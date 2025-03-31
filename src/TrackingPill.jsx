b04f064fa1f1b06f024682a521f98a6839e05a7c
GiangiuseppeTateo <g.tateo@nextome.com>
2025-03-31 16:43:38 +0200

    Align tracking pills

--- src/TrackingPill.jsx
+++ src/TrackingPill.jsx
@@ -2,8 +2,8 @@ import { SmartphoneNfcIcon, TagIcon } from "zudoku/icons"
 
 export function SmartphoneTrackingPill() {
     return <span style={{color:"#5BC4F0", float:"left"}}><SmartphoneNfcIcon style={{float:"left", marginRight:"0.5em"}}/> Smartphone Tracking</span>
-  }
+}
 
-  export function TagTrackingPill() {
-    return <span style={{color:"#7B29EA", float:"left"}}><TagIcon style={{float:"left"}}/> Tag Tracking</span> 
-  }
+export function TagTrackingPill() {
+    return <span style={{color:"#7B29EA", float:"left"}}><TagIcon style={{float:"left", marginRight:"0.5em"}}/> Tag Tracking</span> 
+}
